f131dcded78a796e0b19c0bcde4105a5ef86a396
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-05 15:19:11 +0600

    card details page working

--- my-project/src/components/Pages/CardDetails.jsx
+++ my-project/src/components/Pages/CardDetails.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const CardDetails = () => {
+    const data=useLoaderData();
     const {id}= useParams();
+    const [hotel,setHotel]=useState({});
+
+    useEffect(()=>{
+        if(data){
+            const findData= data.find(d=> d.id == id);
+        setHotel(findData)
+        }
+    },[data, id])
+
+    console.log(hotel)
+    const {image}= hotel;
+
+
+
+
     return (
-        <div>
-            <h1>card details page.... {id}</h1>
+        <div className='grid grid-cols-1 md:grid-cols-12'>
+            <section className='col-span-9'>
+            <div>
+                <img className='' src={image} alt="" />
+            </div>
+            </section>
+
+            <section className='col-span-3'>
+dsfsdfsdfsdfs
+            </section>
+
         </div>
     );
 };
